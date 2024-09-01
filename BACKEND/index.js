1a86cc1b02ff08af41276ac19ba0e5532bd1e194
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2024-09-01 19:55:40 +0500

    established connected with mongodb

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const routes = require('./routes');
 const app=express();
-
+require ('dotenv').config();//it will be used to access .env file through out the project
+require ('./config/db');
 const PORT= process.env.PORT || 3000;
 
 app.use('/api/v1', routes);
